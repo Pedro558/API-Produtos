3dbd69baea5ddc81ddc6d2f14145bd8a2149e4a1
pedro558 <pedroafonso109@gmail.com>
2025-10-22 20:24:48 -0300

    :recycle: refactor: update app structure

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
-import swaggerUi from "swagger-ui-express";
+//import swaggerUI from "swagger-ui-express";
+//import swaggerSpec from "./docs/swagger.ts"
+import clienteRoutes from "./routes/clienteRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/clientes", clienteRoutes);
+
+// Swagger UI
+//app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.get("/health", (req, res) => {
   res.send({ status: "ok" });
