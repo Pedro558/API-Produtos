ba3ef9faf91fa17f15dc3005d2f1357972a4749d
pedro558 <pedroafonso109@gmail.com>
2025-10-25 11:51:47 -0300

    :sparkles: feat: extend clienteRoutes with additional CRUD operations and search functionality

--- src/routes/clienteRoutes.ts
+++ src/routes/clienteRoutes.ts
@@ -4,6 +4,11 @@ import * as controller from "../controllers/clienteController.js"
 const  router = Router()
 
 router.get("/", controller.listar)
+router.get("/contar", controller.contar)
+router.get("/nome/:nome", controller.buscaPorNome)
+router.get("/:id", controller.buscaId)
 router.post("/", controller.criar)
+router.put("/:id", controller.atualizar)
+router.delete("/:id", controller.remover)
 
 export default router
