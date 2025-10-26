466501eb33ec9ea3189c48540c7a7eef912210ca
pedro558 <pedroafonso109@gmail.com>
2025-10-26 16:58:01 -0300

    :recycle: refactor: refactor clienteService to use Prisma for database operations

--- src/services/clienteService.ts
+++ src/services/clienteService.ts
@@ -1,42 +1,51 @@
 import type { Cliente } from '../models/cliente.js'
+import prisma from "../prismaClient.js"
 
-let clientes: Cliente[] = []
-let currentId = 1
-
-export const listarTodos = async(): Promise<Cliente[]> => clientes
+export const listarTodos = async(): Promise<Cliente[]> => {
+  return prisma.cliente.findMany()
+}
 
 export const buscarPorId = async(id: number): Promise<Cliente | null> => {
-  return clientes.find(cliente => cliente.id === id) || null
+  return prisma.cliente.findUnique({ where: { id }})
 }
 
 export const buscarPorNome = async(nome: string): Promise<Cliente[]> => {
-  return clientes.filter(cliente => cliente.nome.toLowerCase().includes(nome.toLowerCase()))
+  return prisma.cliente.findMany({
+    where: {
+      nome: {
+        contains: nome,
+        mode: "insensitive"
+      },
+    },
+  });
 }
 
-export const contar = async(): Promise<number> => clientes.length
+export const contar = async(): Promise<number> => {
+  return prisma.cliente.count()
+}
 
 export const salvar = async(data:Cliente): Promise<Cliente> =>{
-  const novo: Cliente = {
-    ...data,
-    id: currentId++,
-    createdAt: new Date()
-  }
-  clientes.push(novo)
-  return novo
+  return prisma.cliente.create({ data}) 
 }
 
 export const atualizar = async(id: number, data: Partial<Cliente>): Promise<Cliente | null> => {
-  const userId = clientes.findIndex(cliente => cliente.id ===id)
-  if (userId === -1) return null
-  clientes[userId] = {
-    ...clientes[userId],
-    ...data
+  try {
+    return prisma.cliente.update({
+      where: { id },
+      data
+    })
+    
+  } catch (error) {
+    return null
   }
-  return clientes[userId]
 }
 
 export const deletar = async(id: number): Promise<boolean> => {
-  const initialLength = clientes.length
-  clientes = clientes.filter(cliente => cliente.id !== id)
-  return clientes.length < initialLength
+  try{
+    await prisma.cliente.delete({ where: { id }})
+    return true
+  } catch(error){
+      return false
+    }
+
 }
