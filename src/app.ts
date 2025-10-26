13314761a5e287bf62af4fd9803e85c22b0031b1
pedro558 <pedroafonso109@gmail.com>
2025-10-26 16:20:43 -0300

    :sparkles: feat: add swagger configuration

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-//import swaggerUI from "swagger-ui-express";
-//import swaggerSpec from "./docs/swagger.ts"
+import swaggerUI from "swagger-ui-express";
+import swaggerSpec from "./docs/swagger.js"
 import clienteRoutes from "./routes/clienteRoutes.js";
 
 const app = express();
@@ -9,11 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/clientes", clienteRoutes);
 
-// Swagger UI
-//app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.get("/health", (req, res) => {
   res.send({ status: "ok" });
